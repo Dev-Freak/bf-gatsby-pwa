8b8ae53d5c59a6bb59a632015ddef0f62d875eaf
DevFreak <hmengineer@outlook.com>
2020-10-16 01:08:46 +1100

    Add icons file to corectly re-export all the tile's icons

---
+++ src/utils/icons.js
@@ -0,0 +1,9 @@
+export const CommercialLogo = require("../images/icons/Icon1.svg")
+export const OtherLogo = require("../images/icons/Icon5.svg")
+export const ResidentialLogo = require("../images/icons/Icon8.svg")
+export const AssetsFinancialLogo = require("../images/icons/Icon39.svg")
+
+export const OneApplicant = require("../images/icons/Icon34.svg")
+export const TwoApplicant = require("../images/icons/Icon31.svg")
+export const ThreeApplicant = require("../images/icons/Icon33.svg")
+export const FourApplicant = require("../images/icons/Icon32.svg")
