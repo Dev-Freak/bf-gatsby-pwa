d34ac66a6963ec01913b7c99f65b9a5967a5d01f
DevFreak <hmengineer@outlook.com>
2020-12-01 13:37:48 +1100

    Storybook version update 6.0.26 -> 6.1.9

--- src/stories/stepper/Tile.stories.tsx
+++ src/stories/stepper/Tile.stories.tsx
@@ -11,14 +11,17 @@ export default {
   component: Tile,
 } as Meta
 
-const Template: Story<TileProps> = args => (
-  <StateProvider>
-    <Tile {...args} />
-  </StateProvider>
-)
+const Template: Story<TileProps> = args => <Tile {...args} />
 
-export const Default = Template.bind({})
-Default.args = {
+export const Idle = Template.bind({})
+Idle.args = {
   img: Logo,
   children: "Commercial",
+}
+
+export const Selected = Template.bind({})
+Selected.args = {
+  img: Logo,
+  children: "Commercial",
+  selected: true,
 }
