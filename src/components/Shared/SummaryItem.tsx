67db0dd987f9f58c4ce795b1733a7076950d354c
DevFreak <hmengineer@outlook.com>
2020-12-11 01:25:46 +1100

    Research into "Why the SummaryItem's values are rendering with a comma inbetween items (,)" TODO: A space needs to be added!

--- src/components/Shared/SummaryItem.tsx
+++ src/components/Shared/SummaryItem.tsx
@@ -6,7 +6,7 @@ import Value from "./Value"
 import { formatKey } from "../../utils/stringFormatter"
 
 type SelectedValue = {
-  [key: string]: string | SelectedValue
+  [key: string]: string
 }
 
 export type SummaryItemProps = {
@@ -18,37 +18,42 @@ const SummaryItem: React.FC<SummaryItemProps> = ({
   itemTitle,
   value,
 }: SummaryItemProps) => {
-  const getComponents = (data: SelectedValue) => {
-    if (data !== null && typeof data === "object")
-      return Object.keys(value).map((key: any) => {
-        const item = data[key]
+  const getComponents = (item: SelectedValue) => {
+    if (item !== null && typeof item === "object")
+      return Object.keys(value).map((itemKey: any) => {
+        const subItemFirstLevel = item[itemKey]
 
-        if (item !== null && typeof item === "object") {
+        if (subItemFirstLevel !== null && typeof subItemFirstLevel === "object") {
           return (
-            <div className="pb-5" key={key}>
-              <Value>{formatKey(key)}</Value>
+            <div className="pb-5" key={itemKey}>
+              <Value>{formatKey(itemKey)}</Value>
               <div className="px-6">
-                {Object.keys(item).map((subKey: any, index) => {
-                  const subItem = item[subKey]
+                {Object.keys(subItemFirstLevel).map(
+                  (subItemFirstLevelKey: any, index) => {
+                    const subItemSecondLevel =
+                      subItemFirstLevel[subItemFirstLevelKey]
 
-                  return (
-                    <Value key={`value-${index}`}>
-                      {`${formatKey(subKey)}: ${subItem}`}
-                    </Value>
-                  )
-                })}
+                    // Use of backtick characters (``) automatically adds the comma WITHOUT space (,) between each selected item
+                    return (
+                      <Value key={`value-${index}`}>
+                        {`${formatKey(subItemFirstLevelKey)}: ${subItemSecondLevel}`}
+                      </Value>
+                    )
+                  }
+                )}
               </div>
             </div>
           )
-        } else
+        } else {
           return (
             <div className="pb-5">
-              <Value>{key}</Value>
+              <Value>{itemKey}</Value>
               <div className="px-6">
-                <Value>{formatKey(item.toString())}</Value>
+                <Value>{formatKey(subItemFirstLevel.toString())}</Value>
               </div>
             </div>
           )
+        }
       })
   }
 
