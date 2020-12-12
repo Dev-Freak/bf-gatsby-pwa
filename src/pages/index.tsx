9ea54b7897e41b4d4ae3b9c023cfad3f515ade74
DevFreak <hmengineer@outlook.com>
2020-12-12 22:51:46 +1100

    Adopts TypeScript pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import React from "react"
-import { Link } from "gatsby"
+//import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/Seo"
 
 import AppFlow from "../components/AppFlow"
