adb338b6d36ca6142638ff854908a94bccfb8ba2
DevFreak <hmengineer@outlook.com>
2020-12-12 23:13:20 +1100

    Changes imports to support ES6 syntax

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -2,79 +2,130 @@
 
 // COMMERCIAL
 // Welcome
-export const CommercialLogo = require("../images/icons/Icon1.svg")
-export const OtherLogo = require("../images/icons/Icon5.svg") // TODO
-export const ResidentialLogo = require("../images/icons/Icon8.svg")
-export const AssetsFinancialLogo = require("../images/icons/Icon39.svg")
+import CommercialLogo from "../images/icons/Icon1.svg"
+import OtherLogo from "../images/icons/Icon5.svg" // TODO
+import ResidentialLogo from "../images/icons/Icon8.svg"
+import AssetsFinancialLogo from "../images/icons/Icon39.svg"
 
 // Applicants Quantity
-export const OneApplicantLogo = require("../images/icons/Icon34.svg")
-export const TwoApplicantLogo = require("../images/icons/Icon31.svg")
-export const ThreeApplicantLogo = require("../images/icons/Icon33.svg")
-export const FourApplicantLogo = require("../images/icons/Icon32.svg")
+import OneApplicantLogo from "../images/icons/Icon34.svg"
+import TwoApplicantLogo from "../images/icons/Icon31.svg"
+import ThreeApplicantLogo from "../images/icons/Icon33.svg"
+import FourApplicantLogo from "../images/icons/Icon32.svg"
 
 // Application Type
-export const RefinanceLogo = require("../images/icons/Icon7.svg")
-export const FirstHomeBuyerLogo = require("../images/icons/Icon10.svg")
-export const InvestorLogo = require("../images/icons/Icon12.svg")
-export const RetirementLogo = require("../images/icons/Icon24.svg")
+import RefinanceLogo from "../images/icons/Icon7.svg"
+import FirstHomeBuyerLogo from "../images/icons/Icon10.svg"
+import InvestorLogo from "../images/icons/Icon12.svg"
+import RetirementLogo from "../images/icons/Icon24.svg"
 
 // Project Type
-export const EstablishedLogo = require("../images/icons/Icon4.svg") // TODO
-export const OffThePlanLogo = require("../images/icons/Icon4.svg")
-export const ConstructionLogo = require("../images/icons/Icon3.svg")
-export const SMSFLogo = require("../images/icons/Icon13.svg")
-export const ReverseMortgageLogo = require("../images/icons/Icon30.svg")
+import EstablishedLogo from "../images/icons/Icon4.svg" // TODO
+import OffThePlanLogo from "../images/icons/Icon4.svg"
+import ConstructionLogo from "../images/icons/Icon3.svg"
+import SMSFLogo from "../images/icons/Icon13.svg"
+import ReverseMortgageLogo from "../images/icons/Icon30.svg"
 
 // Applicant Income Type
-export const PayslipLogo = require("../images/icons/Icon20.svg")
-export const PensionCentrelinkLogo = require("../images/icons/Icon16.svg")
-export const SelfEmployedLogo = require("../images/icons/Icon43.svg")
+import PayslipLogo from "../images/icons/Icon20.svg"
+import PensionCentrelinkLogo from "../images/icons/Icon16.svg"
+import SelfEmployedLogo from "../images/icons/Icon43.svg"
 
 // Applicant Employment Type
-export const CasualLogo = require("../images/icons/Icon36.svg")
-export const PartTimeLogo = require("../images/icons/Icon38.svg")
-export const FullTimeLogo = require("../images/icons/Icon37.svg")
+import CasualLogo from "../images/icons/Icon36.svg"
+import PartTimeLogo from "../images/icons/Icon38.svg"
+import FullTimeLogo from "../images/icons/Icon37.svg"
 
 // Applicant SelfEmployment Type
-export const SoleTraderLogo = require("../images/icons/Icon39.svg")
-export const CompanyLogo = require("../images/icons/Icon2.svg")
-export const TrustEntityLogo = require("../images/icons/Icon26.svg")
+import SoleTraderLogo from "../images/icons/Icon39.svg"
+import CompanyLogo from "../images/icons/Icon2.svg"
+import TrustEntityLogo from "../images/icons/Icon26.svg"
 
 // ASSETS FINANCIAL
 // Assets Type
-export const EquipmentPlanLogo = require("../images/icons/Icon6.svg")
-export const VehiclePurchaseLogo = require("../images/icons/Icon17.svg")
-export const CreditCardLogo = require("../images/icons/Icon22.svg")
-export const CashflowLendingLogo = require("../images/icons/Icon21.svg")
+import EquipmentPlanLogo from "../images/icons/Icon6.svg"
+import VehiclePurchaseLogo from "../images/icons/Icon17.svg"
+import CreditCardLogo from "../images/icons/Icon22.svg"
+import CashflowLendingLogo from "../images/icons/Icon21.svg"
 
 // Assets Purchase
-export const ABNLogo = require("../images/icons/Icon18.svg")
-export const PersonalNameLogo = require("../images/icons/Icon35.svg")
+import ABNLogo from "../images/icons/Icon18.svg"
+import PersonalNameLogo from "../images/icons/Icon35.svg"
 
 // COMMERCIAL
 // Commercial Type
-export const PurchaseLogo = require("../images/icons/Icon28.svg")
-export const Refinance_2Logo = require("../images/icons/Icon25.svg")
-export const DevelopmentLogo = require("../images/icons/Icon15.svg")
+import PurchaseLogo from "../images/icons/Icon28.svg"
+import Refinance_2Logo from "../images/icons/Icon25.svg"
+import DevelopmentLogo from "../images/icons/Icon15.svg"
 
 // Development Type
-export const DANotAppliedLogo = require("../images/icons/Icon42.svg")
-export const DAPendingLogo = require("../images/icons/Icon41.svg")
-export const DAApprovedLogo = require("../images/icons/Icon40.svg")
+import DANotAppliedLogo from "../images/icons/Icon42.svg"
+import DAPendingLogo from "../images/icons/Icon41.svg"
+import DAApprovedLogo from "../images/icons/Icon40.svg"
 
 // REST
-export const BuiltHomeLogo = require("../images/icons/Icon5.svg")
-export const EquityReleaseLogo = require("../images/icons/Icon9.svg")
-export const PropertyPriceBalanceLogo = require("../images/icons/Icon11.svg")
-export const ConsolidateLogo = require("../images/icons/Icon14.svg")
-export const FactoryLogo = require("../images/icons/Icon19.svg")
-export const LandLogo = require("../images/icons/Icon23.svg")
-export const BetterRateLogo = require("../images/icons/Icon27.svg")
+import BuiltHomeLogo from "../images/icons/Icon5.svg"
+import EquityReleaseLogo from "../images/icons/Icon9.svg"
+import PropertyPriceBalanceLogo from "../images/icons/Icon11.svg"
+import ConsolidateLogo from "../images/icons/Icon14.svg"
+import FactoryLogo from "../images/icons/Icon19.svg"
+import LandLogo from "../images/icons/Icon23.svg"
+import BetterRateLogo from "../images/icons/Icon27.svg"
 
 // DUPLICATE NAMESLogo => A better use case must be given to these
-export const VehiclePurchase_2Logo = require("../images/icons/Icon29.svg")
+import VehiclePurchase_2Logo from "../images/icons/Icon29.svg"
 
 // REPORT ICONS
-export const ReportSVG = require("../images/svgs/report.svg")
-export const ScreenSVG = require("../images/svgs/screen.svg")
+import ReportSVG from "../images/svgs/report.svg"
+import ScreenSVG from "../images/svgs/screen.svg"
+
+export {
+  CommercialLogo,
+  OtherLogo,
+  ResidentialLogo,
+  AssetsFinancialLogo,
+  OneApplicantLogo,
+  TwoApplicantLogo,
+  ThreeApplicantLogo,
+  FourApplicantLogo,
+  RefinanceLogo,
+  FirstHomeBuyerLogo,
+  InvestorLogo,
+  RetirementLogo,
+  EstablishedLogo,
+  OffThePlanLogo,
+  ConstructionLogo,
+  SMSFLogo,
+  ReverseMortgageLogo,
+  PayslipLogo,
+  PensionCentrelinkLogo,
+  SelfEmployedLogo,
+  CasualLogo,
+  PartTimeLogo,
+  FullTimeLogo,
+  SoleTraderLogo,
+  CompanyLogo,
+  TrustEntityLogo,
+  EquipmentPlanLogo,
+  VehiclePurchaseLogo,
+  CreditCardLogo,
+  CashflowLendingLogo,
+  ABNLogo,
+  PersonalNameLogo,
+  PurchaseLogo,
+  Refinance_2Logo,
+  DevelopmentLogo,
+  DANotAppliedLogo,
+  DAPendingLogo,
+  DAApprovedLogo,
+  BuiltHomeLogo,
+  EquityReleaseLogo,
+  PropertyPriceBalanceLogo,
+  ConsolidateLogo,
+  FactoryLogo,
+  LandLogo,
+  BetterRateLogo,
+  VehiclePurchase_2Logo,
+  ReportSVG,
+  ScreenSVG,
+}
