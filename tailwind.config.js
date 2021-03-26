9bd1542e7355ab2ce0e8afa3e285d8b283c02f1f
DevFreak <hmengineer@outlook.com>
2021-03-26 19:38:50 +1100

    Tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ["./src/**/*.html", "./src/**/*.tsx"],
   theme: {
     fontFamily: {
       poppins: ["Poppins", "sans-serif"],
